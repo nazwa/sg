a132024fd3c7a48250a93a59666c423aade322ff
Maciej Ka <zorleq@hotmail.com>
2016-02-21 02:54:06 +0000

    "Updates...

--- main.go
+++ main.go
@@ -11,25 +11,24 @@ func isInGit(wd string) bool {
 	cmd := exec.Command("git", "rev-parse")
 	cmd.Dir = wd
 	output, err := cmd.CombinedOutput()
-	if err != nil {
-		panic(err)
-	}
-	return len(output) == 0
+	return len(output) == 0 && err == nil
 }
 
 func main() {
 	cwd, _ := os.Getwd()
-	fmt.Println(isInGit(cwd))
+	if !isInGit(cwd) {
+		fmt.Println("This is not a git repo :(")
+		return
+	}
 
-	message := ""
-
+	message := "Updates..."
 	if len(os.Args) > 1 {
 		words := os.Args[1:]
 		message = strings.Join(words, " ")
 	}
+
 	addCmd := exec.Command("git", "add", "-A")
 	addCmd.Dir = cwd
-
 	if err := addCmd.Run(); err != nil {
 		fmt.Println(err.Error())
 		return
@@ -37,12 +36,22 @@ func main() {
 
 	commitCmd := exec.Command("git", "commit", "-am\""+message)
 	commitCmd.Dir = cwd
-
 	if err := commitCmd.Run(); err != nil {
 		fmt.Println(err.Error())
 		return
 	}
 
-	fmt.Println(cwd)
-	fmt.Println(message)
+	pullCmd := exec.Command("git", "pull")
+	pullCmd.Dir = cwd
+	if err := pullCmd.Run(); err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	pushCmd := exec.Command("git push")
+	pushCmd.Dir = cwd
+	if err := pushCmd.Run(); err != nil {
+		fmt.Println(err.Error())
+		return
+	}
 }
